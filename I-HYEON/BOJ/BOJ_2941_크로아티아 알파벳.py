f014c48179dad25447dd2a927e128aa0adfa9224
ihyn <tjwn1408@naver.com>
2023-02-05 19:50:16 +0900

    [BOJ] 크로아티아 알파벳  / 실버5
    https://www.acmicpc.net/problem/2941

---
+++ I-HYEON/BOJ/BOJ_2941_크로아티아 알파벳.py
@@ -0,0 +1,12 @@
+# 크로아티아 알파벳을 변경한 문자를 담은 리스트를 만든다.
+# 문자열을 입력받는다.
+# 리스트를 돌면서 문자열 내에 해당 문자가 있으면 'a'로 변경한다.
+# 최종적으로 변경된 문자열의 길이를 반환한다.
+
+string = input()
+croa_li = ['c=','c-','dz=','d-','lj','nj','s=','z=']
+
+for i in croa_li:
+    string = string.replace(i,'a')
+
+print(len(string))
