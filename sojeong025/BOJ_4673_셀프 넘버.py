da4148067cd483c7a25ceee1c1c51ba6c295a1df
sojeong <thwjd025@gmail.com>
2023-02-11 22:49:10 +0900

    [BOJ] 단어공부/ 브론즈1
    https://www.acmicpc.net/problem/1157

--- sojeong025/BOJ_4673_셀프 넘버.py
+++ sojeong025/BOJ_4673_셀프 넘버.py
@@ -1,13 +1,15 @@
 def self_number(num):
-
     self_num=num
     while num != 0:
+        # 오른쪽 끝 숫자를 더함
         self_num += num % 10
+        # 오른쪽 끝 숫자를 삭제
         num //= 10
     return self_num
 
 answer=[]
 for i in range(1,10001):
     answer.append(self_number(i))
+    # 생성자가 없는 것이 셀프 넘버
     if i not in answer:
         print(i)
