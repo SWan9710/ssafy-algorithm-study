451c91c1384fbe5a4cf6ac8975c8a678333287a6
SWan9710 <wjdtndhks1212@gmail.com>
2023-02-14 23:40:11 +0900

    [BOJ]듣보잡/실버4
    https://www.acmicpc.net/problem/1764

--- SWan9710/SEA/SEA_1974_스도쿠 검증.py
+++ SWan9710/SEA/SEA_1974_스도쿠 검증.py
@@ -12,6 +12,7 @@ for tc in range(1,test_case+1):
     # print(sdoku_arr)
 
     # 스도쿠 90도 회전
+    # 전치행렬이다.
     rotation_sdoku = [[0] * 9 for _ in range(9)]
     for i in range(9):
         for j in range(9):
@@ -25,9 +26,9 @@ for tc in range(1,test_case+1):
             # 블럭으로 가져오는 스도쿠를 저장할 공간
             temp_sdoku = []
 
-            for k in range(-1, 2):  # k >> -1, 0, 1
-                for m in range(-1, 2):  # m >> -1, 0, 1
-
+            for k in range(-1, 2):  # k >> -1, 0, 1                 # 0,0 / 0,1 / 0,2
+                for m in range(-1, 2):  # m >> -1, 0, 1             # 1,0 / 1,1 / 1,2
+                                                                    # 2,0 / 2,1 / 2,2
                     # 제일먼저 j값이 바뀌고 그다음 i 값이 바뀜
                     temp_sdoku.append(sdoku_arr[i + k][j + m])
             block_sdoku.append((temp_sdoku))
@@ -38,6 +39,8 @@ for tc in range(1,test_case+1):
         if len(set(sdoku_arr[i])) != 9:
             duplication += 1
             break
+            # 리스트 전체의 값이 45가 아닐때 중복을 늘린다.
+            # 5 5 5  5 5 5  5 5 5 >> 반례가 있다,,,,
 
     # 스도쿠 세로줄 중복 검사하기
         if len(set(rotation_sdoku[i])) != 9:
