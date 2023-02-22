5f8134e6ee9557588f0d5a9b2a4064aa61f96a1e
SWan9710 <wjdtndhks1212@gmail.com>
2023-02-23 00:10:34 +0900

    [BOJ]단어나누기/실버5
    https://www.acmicpc.net/problem/1251

--- SWan9710/BOJ/[BOJ]단어나누기.py
+++ SWan9710/BOJ/[BOJ]단어나누기.py
@@ -2,13 +2,16 @@
 word = input()
 n = len(word)
 word_list = []
-
+# 1부터 단어길이 -1 까지
 for i in range(1, n-1):
     for j in range(i+1, n):
-        left = word[:i]
-        middle = word[i:j]
-        right = word[j:]
-        changed = left[::-1] + middle[::-1] + right[::-1]
-        word_list.append(changed)
-
-print(word_list)
+        # 처음부터 i까지
+        L_word = word[:i]
+        # i부터 j까지
+        M_word = word[i:j]
+        # j부터 끝까지
+        R_word = word[j:]
+        result = L_word[::-1] + M_word[::-1] + R_word[::-1]
+        word_list.append(result)
+word_list.sort()
+print(word_list[0])
