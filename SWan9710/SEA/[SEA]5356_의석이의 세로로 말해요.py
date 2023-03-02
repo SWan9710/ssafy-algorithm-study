4198c8d0e0d43eaefa4baa8b308f023cf1eabe11
SWan9710 <wjdtndhks1212@gmail.com>
2023-03-02 22:52:18 +0900

    [SEA]의석이의 세로로 말해요 / D3

---
+++ SWan9710/SEA/[SEA]5356_의석이의 세로로 말해요.py
@@ -0,0 +1,25 @@
+import sys
+sys.stdin = open('input.txt')
+
+T = int(input())
+for tc in range(1, T+1):
+    arr = [list(input()) for _ in range(5)]
+
+    length = 0
+    for i in range(5):
+        if len(arr[i]) > length:
+            length = len(arr[i])
+
+    for i in range(5):
+        while len(arr[i]) < length:
+            arr[i].append(-1)
+    arr2 = list(map(list, zip(*arr)))
+
+    result = ''
+    for i in range(len(arr2)):
+        for j in arr2[i]:
+            if j == -1:
+                continue
+            else:
+                result += j
+    print(f'#{tc}',result)
